597ec69442f6f2b9582c508bbfda4545777d946f
Gerrit Balster <Gerrit.Balster@atruvia.de>
2023-03-14 11:44:49 +0100

    modified:   JavaNewFeatures/.settings/org.eclipse.core.resources.prefs 	deleted:    JavaNewFeatures/JavaNewFeatures.launch 	modified:   Log4JAndMockitoTests/src/junittest/JUnitTestA.java

--- Log4JAndMockitoTests/src/junittest/JUnitTestA.java
+++ Log4JAndMockitoTests/src/junittest/JUnitTestA.java
@@ -5,6 +5,8 @@ package junittest;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.regex.PatternSyntaxException;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +19,7 @@ class JUnitTestA extends JUnitTestBase {
 	@Test
 	void test1() {
 		debug ("JUnitTestA_test1");
-		// Verwendung einer Methodenreferenz f�hrt hier zu Nullpointer-Exception
+		// Verwendung einer Methodenreferenz führt hier zu einer Exception
 		assertThrows (NullPointerException.class, () -> ((String) null).chars());
 	}
 	
@@ -25,16 +27,16 @@ class JUnitTestA extends JUnitTestBase {
 	@Test
 	void test2() {
 		debug ("JUnitTestA_test2");
-		// Verwendung einer Methodenreferenz f�hrt hier zu Nullpointer-Exception
-		assertThrows (IllegalArgumentException.class, () -> ((String) null).chars());
+		// Verwendung einer Methodenreferenz führt hier zu einer Exception
+		assertThrows (PatternSyntaxException.class, () -> "abc".matches ("["));
 	}
 
 	@Tag ("JUnitTestA_test3")
 	@Test
 	void test3() {
 		debug ("JUnitTestA_test3");
-		// Verwendung einer Methodenreferenz f�hrt hier zu Nullpointer-Exception
-		assertThrows (IllegalArgumentException.class, () -> ((String) null).chars());
+		// Verwendung einer Methodenreferenz führt hier zu einer Exception
+		assertThrows (IllegalArgumentException.class, () -> Integer.parseInt ("keine Zahl"));
 	}
 
 }
